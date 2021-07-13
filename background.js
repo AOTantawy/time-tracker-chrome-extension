6f763e381155d0dd0dd1cc18261a1f241e60d3bb
aotantawy <ahmedosama9100@gmail.com>
2021-07-13 09:31:17 +0200

    add background.js and popup.js

---
+++ background.js
@@ -0,0 +1,55 @@
+let hr = 0;
+let min = 0;
+let sec = 0;
+let interval;
+chrome.action.setBadgeText({ text: `0` });
+chrome.action.setBadgeBackgroundColor({ color: "#4688F1" });
+
+chrome.runtime.onConnect.addListener(function (port) {
+  port.onMessage.addListener(function (msg) {
+    
+    if (msg.start === "start" ) {
+      console.log("start");
+      manageInterval(port);
+    } else if (msg.pause === "pause") {
+      console.log("pause");
+      clearInterval(interval);
+      port.postMessage({ hr, min, sec });
+    } else {
+      console.log("reset");
+      clearInterval(interval);
+      hr = 0;
+      min = 0;
+      sec = 0;
+      port.postMessage({ hr, min, sec });
+    }
+
+  });
+});
+
+function manageInterval(port) {
+  clearInterval(interval);
+  interval = setInterval(() => {
+    sec++;
+    if (sec > 60) {
+      sec = 0;
+      min++;
+    }
+    if (min > 60) {
+      min = 0;
+      hr++;
+    }
+    try {
+      port.postMessage({ hr, min, sec });
+      console.log("connected Port");
+    } catch {
+      console.log("disconnected port");
+    }
+    updateBadge(hr,min);
+  }, 1000);
+}
+
+
+function updateBadge(hr,min) {
+    chrome.action.setBadgeText({ text: `${hr * 60 + min}` });
+}
---
+++ popup.js
@@ -0,0 +1,29 @@
+const port = chrome.runtime.connect({ name: "counter" });
+
+const timeCounter = document.querySelector("#time-counter");
+const start = document.querySelector("#start");
+const pause = document.querySelector("#pause");
+const reset = document.querySelector("#reset");
+
+document.addEventListener("DOMContentLoaded" , () => {
+    port.postMessage({ pause: "pause" });
+})
+
+start.onclick = () => {
+    port.postMessage({start:"start"});
+}
+
+pause.onclick = () => {
+  port.postMessage({ pause:"pause" });
+};
+
+reset.onclick = () => {
+  port.postMessage({ reset:"reset" });
+};
+
+
+port.onMessage.addListener(function (msg) {
+    timeCounter.innerHTML = `${msg.hr} hr: ${msg.min} min: ${msg.sec} sec`;
+});
+
+
